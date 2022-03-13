ce64faac3e66277780da8c9f77fa3198446081ac
xRAIDENx <priyanshupanda.ctp@gmail.com>
2022-03-13 21:05:59 +0530

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,8 +14,8 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory();
-  const greeter = await Greeter.deploy();
+  const Greeter = await hre.ethers.getContractFactory("DragonToken");
+  const greeter = await Greeter.deploy("DragonToken", 'DT');
 
   await greeter.deployed();
 
